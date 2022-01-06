c396586cf4b69b85c1764528249108f6f538e9ee
daniel <52607370+danielrlsoares@users.noreply.github.com>
2022-01-06 10:35:09 -0300

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,15 +1,6 @@
 <?php
 //Conexão com banco de dados
-$servername = "ec2-3-211-240-42.compute-1.amazonaws.com"; //endereço do servidor
-$username="duevzcajranagt";
-$password="658b5f4795b11838c7490f9d231a646ba08fcfe4370d1f72ec57b12649695f54";
-$db_name="d9h1pdr1idnnl";
-
-//pdo - somente orientado objeto
-$connect =mysqli_connect($servername,$username,$password,$db_name);
-
-//codifica com o caracteres ao manipular dados do banco de dados
-//mysqli_set_charset($connect, "utf8");
+$con = pg_connect(getenv("postgres://duevzcajranagt:658b5f4795b11838c7490f9d231a646ba08fcfe4370d1f72ec57b12649695f54@ec2-3-211-240-42.compute-1.amazonaws.com:5432/d9h1pdr1idnnl"));
 
 if(mysqli_connect_error()):
 	echo "Falha na conexão: ". mysqli_connect_error();
