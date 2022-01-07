0e9ccf8337e8661a5645981d710b7e3ec592fb08
daniel <52607370+danielrlsoares@users.noreply.github.com>
2022-01-07 13:17:38 -0300

    Update create_associacao.php

--- create_associacao.php
+++ create_associacao.php
@@ -3,12 +3,7 @@
 	//Iniciar  Sessão
 	session_start();
 	
-	$log  = "User: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
-        "Attempt: ".($result[0]['success']=='1'?'Success':'Failed').PHP_EOL.
-        "User: ".$username.PHP_EOL.
-        "-------------------------".PHP_EOL;
-	//Save string to log, use FILE_APPEND to append.
-	file_put_contents('./log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
+	error_log("a sessão deve ter sido iniciada",0);
 
 	if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['telefone']) && isset($_POST['senha'])){
 		$nome=mysqli_escape_string($connect,$_POST['nome']);
@@ -16,22 +11,12 @@
 		$telefone=mysqli_escape_string($connect,$_POST['telefone']);
 		$senha=mysqli_escape_string($connect,$_POST['senha']);
 		
-		$log  = "User: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
-		"Attempt: ".($result[0]['success']=='1'?'Success':'Failed').PHP_EOL.
-		"User: ".$username.PHP_EOL.
-		"-------------------------".PHP_EOL;
-		//Save string to log, use FILE_APPEND to append.
-		file_put_contents('./log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
+		error_log("entramos no if",0)
 		
 		$con = pg_connect(getenv("DATABASE_URL"));
 		$sql="INSERT INTO associacao(nome_associacao,email_associacao,telefone_associacao, senha_associacao) VALUES ('$nome', '$email', '$telefone', '$senha')";
 		
-		$log  = "User: ".$_SERVER['REMOTE_ADDR'].' - '.date("F j, Y, g:i a").PHP_EOL.
-		"Attempt: ".($result[0]['success']=='1'?'Success':'Failed').PHP_EOL.
-		"User: ".$username.PHP_EOL.
-		"-------------------------".PHP_EOL;
-		//Save string to log, use FILE_APPEND to append.
-		file_put_contents('./log_'.date("j.n.Y").'.log', $log, FILE_APPEND);
+		error_log("passamos pela conexão e inserção de dados",0)
 		
 		echo $sql;
 		if(pg_query($con, $sql)){
