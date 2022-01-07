5839f6cb39e7ee7742e9ddfd684f56cc19a7aa02
daniel <52607370+danielrlsoares@users.noreply.github.com>
2022-01-07 13:32:41 -0300

    Update create_associacao.php

--- create_associacao.php
+++ create_associacao.php
@@ -6,19 +6,21 @@
 	error_log("a sessão deve ter sido iniciada",0);
 
 	if (isset($_POST['nome']) && isset($_POST['email']) && isset($_POST['telefone']) && isset($_POST['senha'])){
-		$nome=mysqli_escape_string($connect,$_POST['nome']);
-		$email=mysqli_escape_string($connect,$_POST['email']);
-		$telefone=mysqli_escape_string($connect,$_POST['telefone']);
-		$senha=mysqli_escape_string($connect,$_POST['senha']);
 		
 		error_log("entramos no if",0);
 		
 		$con = pg_connect(getenv("DATABASE_URL"));
+		
+		$nome=mysqli_escape_string($con,$_POST['nome']);
+		$email=mysqli_escape_string($con,$_POST['email']);
+		$telefone=mysqli_escape_string($con,$_POST['telefone']);
+		$senha=mysqli_escape_string($con,$_POST['senha']);
+		
 		$sql="INSERT INTO associacao(nome_associacao,email_associacao,telefone_associacao, senha_associacao) VALUES ('$nome', '$email', '$telefone', '$senha')";
 		
-		error_log("passamos pela conexão e inserção de dados",0);
-		
 		echo $sql;
+		
+		error_log("passamos pela conexão e inserção de dados",0);
 		if(pg_query($con, $sql)){
 			$_SESSION['mensagem'] = "Cadastro com sucesso!";
 			pg_close($con);
