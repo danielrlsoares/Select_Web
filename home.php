a2cc27b62a0ef77989cef77afd3cf942191dc6f7
daniel <52607370+danielrlsoares@users.noreply.github.com>
2022-01-24 14:59:27 -0300

    Update home.php

--- home.php
+++ home.php
@@ -53,18 +53,30 @@ $erros=array();
 			</thead>
 			<tbody>
 				<?php
-				$sql0="SELECT * FROM retirada WHERE pendente=true ORDER BY data_hora_solicitacao ASC";
+				$sql0="SELECT ret.* FROM retirada ret WHERE pendente=true ORDER BY data_hora_solicitacao ASC";
 				$resultado0=pg_query($connect, $sql0);
-				while($dados_ret = pg_fetch_array($resultado0)):
-					$id_soli=$dados_ret['cod_solicitacao'];
-					$sql1="SELECT EXTRACT(day FROM intervalo)*60*24 + EXTRACT(hour FROM intervalo)*60 + EXTRACT(minute FROM intervalo) as demora FROM (SELECT CURRENT_TIMESTAMP-data_hora_solicitacao intervalo FROM retirada WHERE cod_solicitacao=$id_soli) as teste";
-					$demora_array = pg_query($connect, $sql1);
-					$demora=pg_fetch_array($demora_array);
-					
-					$email_usu=$dados_ret['usuario_email_usuario'];
-					$sql2="SELECT nome_usuario, cpf_usuario FROM usuario WHERE email_usuario='$email_usu'";
-					$resultado1 = pg_query($connect, $sql2);
-					$dados_usu = pg_fetch_array($resultado1);
+				$dados_ret = pg_fetch_array($resultado0);
+				
+				$sqla="SELECT bairro FROM endereco WHERE cod_endereco=$id";
+				$resultado=pg_query($connect, $sqla);
+				$bairro_ass = pg_fetch_array($resultado);
+				
+				$id_soli=$dados_ret['cod_solicitacao'];
+				$sqlb="SELECT bairro FROM endereco WHERE cod_endereco=$id";
+				$resultado=pg_query($connect, $sqlb);
+				$bairro_soli = pg_fetch_array($resultado);
+				
+				if($bairro_ass==bairro_soli):
+					while($dados_ret = pg_fetch_array($resultado0)):
+						$id_soli=$dados_ret['cod_solicitacao'];
+						$sql1="SELECT EXTRACT(day FROM intervalo)*60*24 + EXTRACT(hour FROM intervalo)*60 + EXTRACT(minute FROM intervalo) as demora FROM (SELECT CURRENT_TIMESTAMP-data_hora_solicitacao intervalo FROM retirada WHERE cod_solicitacao=$id_soli) as nome";
+						$demora_array = pg_query($connect, $sql1);
+						$demora=pg_fetch_array($demora_array);
+						
+						$email_usu=$dados_ret['usuario_email_usuario'];
+						$sql2="SELECT nome_usuario, cpf_usuario FROM usuario WHERE email_usuario='$email_usu'";
+						$resultado1 = pg_query($connect, $sql2);
+						$dados_usu = pg_fetch_array($resultado1);
 				?>
 				<tr class="consulta_row">
 					<td rowspan=2><img src="<?php echo $dados_ret['foto_local']; ?>" height="55"></td>
@@ -93,8 +105,9 @@ $erros=array();
 					</td>	
 				</tr>
 				<tr class="consulta_row"><td><?php echo $dados_usu['cpf_usuario']; ?></td></tr>
-				<?php 
-				endwhile;
+				<?php
+				 endif;
+					endwhile;
 				pg_close($connect);
 				?>
 			</tbody>
