714ce5328c55dc5df6c28bb1552facbe9996da9a
daniel <52607370+danielrlsoares@users.noreply.github.com>
2022-01-27 16:41:28 -0300

    Update delete_catador.php

--- delete_catador.php
+++ delete_catador.php
@@ -3,15 +3,20 @@ require_once 'dbconnect.php';
 session_start();
 
 if(isset($_GET['id'])):
-	$id = $_GET['id'];	
-	$sql="DELETE FROM catador WHERE cod_catador=$id";
-	
+	$id = $_GET['id'];
+	$sql="DELETE FROM realiza_retirada WHERE catador_cod_catador=$id";
 	if(pg_query($connect, $sql)):
+		$sql="DELETE FROM catador WHERE cod_catador=$id";
+		if(pg_query($connect, $sql)):
 			$_SESSION['mensagem'] = "Deletado com sucesso!";
 			header('Location: catadores.php');
 		else:
 			$_SESSION['mensagem'] = "Erro ao deletar!";
 			die(pg_last_error($connect));
+		endif;
+	else:
+		$_SESSION['mensagem'] = "Erro ao deletar!";
+		die(pg_last_error($connect));
 	endif;
 endif
 ?>
