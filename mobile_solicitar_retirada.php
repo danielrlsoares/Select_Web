e7698d0f3f2ef74f5b4e81dda88a8c57c27e00b3
Haisla <87146767+HaislaEG@users.noreply.github.com>
2022-01-28 03:52:49 -0300

    Update mobile_solicitar_retirada.php

--- mobile_solicitar_retirada.php
+++ mobile_solicitar_retirada.php
@@ -23,32 +23,37 @@ elseif(isset( $_SERVER['HTTP_AUTHORIZATION'])) {
 
 // Se a autenticação não foi enviada
 if(is_null($username)) {
-    $response["success"] = 0;
+	$response["success"] = 0;
 	$response["error"] = "faltam parametros";
 }
 // Se houve envio dos dados
 else {
+	if ( isset($_POST['rua']) && isset($_POST["bairro"]) && isset($_POST["cidade"]) && isset($_POST["numero"]) && isset($_POST["uf"]) && isset($_POST["cep"]) && isset($_POST["referencia"]) && isset($_POST["material"]) && isset($_FILES["foto"]) ) {
+
+		$response["success"] = 1;
+
+		$rua = trim($_POST['rua']);
+		$bairro = trim($_POST['bairro']);
+		$cidade = trim($_POST['cidade']);
+		$numero = trim($_POST['numero']);
+		$uf = trim($_POST['uf']);
+		$cep = trim($_POST['cep']);
+		$referencia = trim($_POST['referencia']);
+		$material = trim($_POST['material']);
+
+		$imageFileType = strtolower(pathinfo(basename($_FILES['foto']['name']),PATHINFO_EXTENSION));
+		$image_base64 = base64_encode(file_get_contents($_FILES['foto']['tmp_name']) );
+		$foto = 'data:image/'.$imageFileType.';base64,'.$image_base64;
+
+		$result_endereco = pg_query($con, "INSERT INTO endereco(rua, bairro, cidade, numero, uf, cep, referencia) VALUES('$rua', '$bairro', '$cidade', '$numero', '$uf', '$cep', '$referencia') RETURNING cod_endereco");
+		$id_endco = pg_fetch_array($result_endereco,0)[0];
+		$result_retirada = pg_query($con, "INSERT INTO retirada(material, foto_material, data_hora_solicitacao, endereco_cod_endereco, usuario_email_usuario) VALUES('$material', '$img', now(), $id_endco , $username )");
+	}
+	else{
 	
-//if ( isset($_POST['rua']) ){//&& isset($_POST["bairro"]) && isset($_POST["cidade"]) && isset($_POST["numero"]) && isset($_POST["uf"]) && isset($_POST["cep"]) && isset($_POST["referencia"]) && isset($_POST["material"]) && isset($_FILES["foto"]) ) {
-
-	$response["success"] = 1;
-
-	$rua = trim($_POST['rua']);
-	$bairro = trim($_POST['bairro']);
-	$cidade = trim($_POST['cidade']);
-	$numero = trim($_POST['numero']);
-	$uf = trim($_POST['uf']);
-	$cep = trim($_POST['cep']);
-	$referencia = trim($_POST['referencia']);
-	$material = trim($_POST['material']);
-
-	$imageFileType = strtolower(pathinfo(basename($_FILES['foto']['name']),PATHINFO_EXTENSION));
-	$image_base64 = base64_encode(file_get_contents($_FILES['foto']['tmp_name']) );
-	$foto = 'data:image/'.$imageFileType.';base64,'.$image_base64;
-
-	$result_endereco = pg_query($con, "INSERT INTO endereco(rua, bairro, cidade, numero, uf, cep, referencia) VALUES('$rua', '$bairro', '$cidade', '$numero', '$uf', '$cep', '$referencia') RETURNING cod_endereco");
-	$id_endco = pg_fetch_array($result_endereco,0)[0];
-	$result_retirada = pg_query($con, "INSERT INTO retirada(material, foto_material, data_hora_solicitacao, endereco_cod_endereco, usuario_email_usuario) VALUES('$material', '$img', now(), $id_endco , $username )");
+		response["success"] = 0;
+		$response["error"] = "Error BD: ".pg_last_error($con);
+	}
 
 }
 	
