91b288659301b8efb2fde15bd42f65173ceae4e8
daniel <52607370+danielrlsoares@users.noreply.github.com>
2022-01-28 07:11:13 -0300

    Update home.php

--- home.php
+++ home.php
@@ -70,7 +70,7 @@ $erros=array();
 					$dados_usu = pg_fetch_array($resultado1);
 				?>
 				<tr class="consulta_row">
-					<td rowspan=2><img src="<?php echo $dados_ret['foto_local']; ?>" height="55"></td>
+					<td rowspan=2><img src="<?php echo $dados_ret['foto_material']; ?>" height="55"></td>
 					<td rowspan=2><?php echo $demora['demora']." min"; ?></td>
 					<td><?php echo $dados_usu['nome_usuario']; ?></td>
 					<td rowspan=2><?php echo $dados_ret['material']; ?></td>
