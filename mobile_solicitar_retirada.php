3311c0725adedd73154d078d1e618e69029e178e
Haisla <87146767+HaislaEG@users.noreply.github.com>
2022-01-28 03:26:26 -0300

    Update mobile_solicitar_retirada.php

--- mobile_solicitar_retirada.php
+++ mobile_solicitar_retirada.php
@@ -29,20 +29,20 @@ if(is_null($username)) {
 // Se houve envio dos dados
 else {
 	
-	if ( isset($_POST["rua"]) ){//&& isset($_POST["bairro"]) && isset($_POST["cidade"]) && isset($_POST["numero"]) && isset($_POST["uf"]) && isset($_POST["cep"]) && isset($_POST["referencia"]) && isset($_POST["material"]) && isset($_FILES["foto"]) ) {
+	if ( isset($_POST['rua']) ){//&& isset($_POST["bairro"]) && isset($_POST["cidade"]) && isset($_POST["numero"]) && isset($_POST["uf"]) && isset($_POST["cep"]) && isset($_POST["referencia"]) && isset($_POST["material"]) && isset($_FILES["foto"]) ) {
 	
 		$response["success"] = 1;
 		
-		$rua = trim($_POST["rua"]);
-		$bairro = trim($_POST["bairro"]);
-		$cidade = trim($_POST["cidade"]);
-		$numero = trim($_POST["numero"]);
-		$uf = trim($_POST["uf"]);
-		$cep = trim($_POST["cep"]);
-		$referencia = trim($_POST["referencia"]);
-		$material = trim($_POST["material"]);
+		$rua = trim($_POST['rua']);
+		$bairro = trim($_POST['bairro']);
+		$cidade = trim($_POST['cidade']);
+		$numero = trim($_POST['numero']);
+		$uf = trim($_POST['uf']);
+		$cep = trim($_POST['cep']);
+		$referencia = trim($_POST['referencia']);
+		$material = trim($_POST['material']);
 		
-		$imageFileType = strtolower(pathinfo(basename($_FILES["foto"]["name"]),PATHINFO_EXTENSION));
+		$imageFileType = strtolower(pathinfo(basename($_FILES['foto']['name']),PATHINFO_EXTENSION));
 		$image_base64 = base64_encode(file_get_contents($_FILES['foto']['tmp_name']) );
 		$foto = 'data:image/'.$imageFileType.';base64,'.$image_base64;
 		
