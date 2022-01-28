2c50d55f5f476f7b81a390709d47a171b15170c2
daniel <52607370+danielrlsoares@users.noreply.github.com>
2022-01-27 22:09:24 -0300

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-	$connect = pg_connect("dbname=dailr9leul3p1p host=ec2-3-228-236-221.compute-1.amazonaws.com port=5432 user=voukeobejiosyj password=6ab4e41340e8a97f2e3157e91c876be8cfa971cb236d9a1279d91d14ff1e9b66 sslmode=require");
+	$connect = pg_connect("dbname=db55a8h0e4vljr host=ec2-18-209-169-66.compute-1.amazonaws.com port=5432 user=wyqmbmqacelels password=63900f645cbea12d21dfb004b0eacabb657d72da0d7aa6f7490403d031099e2c sslmode=require");
 
 	if (!$connect):
 		echo "Falha na Conexão: ".pg_last_error();
