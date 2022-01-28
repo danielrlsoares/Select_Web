457479f17200630e1b9578c9959be1c55b170bad
daniel <52607370+danielrlsoares@users.noreply.github.com>
2022-01-28 07:33:46 -0300

    Add files via upload

---
+++ ver_foto.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Select - Foto</title>
+	<meta charset="utf-8">
+</head>
+<?php
+$id=$_GET['id'];
+$sql0="SELECT * FROM retirada WHERE cod_solicitacao=$id";
+$resultado0=pg_query($connect, $sql0);
+$dados_ret = pg_fetch_array($resultado0)
+?>
+
+<body>
+	<img src="<?php echo $dados_ret['foto_material']; ?>" height="100%">
+</body>
+</html>
